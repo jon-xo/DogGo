80ddb275a7c53d3740a21551911bf5dc70cb14d3
Jon Newton <jonn@hey.com>
2021-05-13 10:39:35 -0400

    Setup inital project file, added Owner/Walker model, repository, and interface.

---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogGo.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DogGo.Repositories
+{
+				public interface IOwnerRepository
+				{
+								List<Owner> GetAllOwners();
+								Owner GetOwnerById(int id);
+								Owner AddOwner(Owner owner);
+								Owner UpdateOwner(Owner owner);
+								Owner DeleteOwner(Owner owner);
+				}
+}
